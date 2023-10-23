7c341fcc32d2ac7649a88634f1b55ce1a79cb0d2
kaique grangeiro <kaique.grangeiro@sp.senai.br>
2023-10-23 17:14:32 -0300

    initial commit

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "api-gin/routes"
+
+func main() {
+	routes.HandleRequests()
+}
---
+++ routes/routes.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"api-gin/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func HandleRequests() {
+	r := gin.Default()
+	r.GET("/alunos", controllers.ListaAlunos)
+	r.Run()
+}
